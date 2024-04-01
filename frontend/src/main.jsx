170651b07aa676c865497ba0723279957073d118
SACHIN KUMAR YADAV <sachinsubh880@gmail.com>
2024-04-02 01:05:39 +0530

    initialized tailwind and daisyUI

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,25 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {  createBrowserRouter, RouterProvider} from "react-router-dom";
-import Root from './routes/root'
-import ErrorPage from './error-page'
+
 
 import './index.css'
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Root/>,
-    errorElement: <ErrorPage/>,
-    children: [
-      {
-        
-      }
-    ]
-  }
-])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+
   </React.StrictMode>,
 )
