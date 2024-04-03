b46aec70306ef5c87c9b180164e578b8ed612104
SACHIN KUMAR YADAV <sachinsubh880@gmail.com>
2024-04-04 01:06:05 +0530

    completed messages part

--- frontend/src/components/sidebar/sidebar.jsx
+++ frontend/src/components/sidebar/sidebar.jsx
@@ -1,13 +1,15 @@
 
+import Conversations from './Conversations.jsx';
+import LogoutButton from './LogoutButton.jsx';
 import SearchInput from './SearchInput.jsx';
 
 const Sidebar = () => {
   return (
-    <div>
+    <div className='border-r border-slate-500 p-4 flex flex-col'>
         <SearchInput/>
         <div className='divider px-3'></div>
-        {/* <Conversations/>
-        <Logout/> */}
+        <Conversations/>
+        <LogoutButton/>
     </div>
   )
 }
