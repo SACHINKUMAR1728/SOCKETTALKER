3bb8ced4ce58a853499a7b833f109d5461dcfc7e
SACHIN KUMAR YADAV <sachinsubh880@gmail.com>
2024-05-08 16:29:59 +0530

    .

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,5 +1,5 @@
-import MessageContainer from "../../components/messages/MessageContainer.jsx";
-import Sidebar from "../../components/sidebar/Sidebar.jsx";
+import MessageContainer from "../../components/messages/MessageContainer";
+import Sidebar from "../../components/sidebar/Sidebar";
 
 const Home = () => {
     return (
